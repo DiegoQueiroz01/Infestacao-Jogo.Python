d4a01b1c2b38b67685fd2fc245863922be3f697b
DiegoQueiroz01 <diqueirozs01@gmail.com>
2025-01-22 18:04:36 -0300

    Adicionar imagens e início do código

--- Código do Jogo.py
+++ Código do Jogo.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+import pygame
+
+pygame.init()
+
+x=1200
+y=720
+
+screen = pygame.display.set_mode((x,y))
+pygame.display.set_caption('Sobreviver')
+
+rodando = True
+
+while rodando:
+    for event in pygame.event.get():
+        if event.type - pygame.QUIT:
+            rodando = False
+    pygame.display.update()
